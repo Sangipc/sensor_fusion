e817b5bdbd93abd473c1b8f5ebc34bff292b87bd
Sangipc <sangipc29@gmail.com>
2024-03-07 13:58:31 +0100

    removed commented codes

--- dataprocessing.py
+++ dataprocessing.py
@@ -43,32 +43,6 @@ def process_data(data: dict):
         quats[id] = vqfs[id].getQuat6D()
     return quats
 
-
-def quaternion_inverse(q):
-    # Calculate the inverse of the quaternion
-    return np.array([q[0], -q[1], -q[2], -q[3]])
-
-
-def quaternion_multiply(q1, q2):
-    # Calculate the quaternion product
-    w1, x1, y1, z1 = q1
-    w2, x2, y2, z2 = q2
-    w = w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2
-    x = w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2
-    y = w1 * y2 - x1 * z2 + y1 * w2 + z1 * x2
-    z = w1 * z2 + x1 * y2 - y1 * x2 + z1 * w2
-    return np.array([w, x, y, z])
-
-
-def relative_quaternion(q1, q2):
-    # Calculate the inverse of the first quaternion
-    q1_inv = quaternion_inverse(q1)
-
-    # Calculate the relative quaternion
-    relative_q = quaternion_multiply(q2, q1_inv)
-
-    return relative_q
-
 def calculate_inclination(Q1, Q2):
     #inv_Q2 = qmt.qinv(Q2)
     dictt = qmt.quatProject(Q1, [0, 0, 1])
@@ -87,17 +61,15 @@ def calculate_inclination(Q1, Q2):
 
     relative_quaternions21 = qmt.qmult(resQuat_Q1,qmt.qinv(resQuat_Q2))
     relative_quaternions12 = qmt.qmult(qmt.qinv(resQuat_Q2), resQuat_Q1)
-    #relative_quaternions = relative_quaternion(Q1, Q2)
     heading_1, inclination_1 = qmt.headingInclinationAngle(resQuat_Q1)
     heading_2, inclination_2 = qmt.headingInclinationAngle(resQuat_Q2)
     heading_3, inclination_21 = qmt.headingInclinationAngle(relative_quaternions21)
-    heading_3, inclination_12 = qmt.headingInclinationAngle(relative_quaternions12)
-    #inclination_3 = abs(inclination_1 - inclination_2)
+    #heading_3, inclination_12 = qmt.headingInclinationAngle(relative_quaternions12)
     inclination_1 = np.degrees(inclination_1)
     inclination_2 = np.degrees(inclination_2)
     inclination_21 = np.degrees(inclination_21)
-    inclination_12 = np.degrees(inclination_12)
-    return inclination_1, inclination_2, inclination_21, inclination_12
+    #inclination_12 = np.degrees(inclination_12)
+    return inclination_1, inclination_2, inclination_21
 
 def always_process_data(sensor_dict):
     global calls_per_second
@@ -114,11 +86,10 @@ def always_process_data(sensor_dict):
         # {'gyro': array([ 0.21070607, -0.36701715, -0.39044639]), 'acc': array([-0.12048834, -0.07399426,  9.43940926])}
         quats = process_data(data)  #this gives me 2 quaternions one for each sensor
         #print(quats)    #{0: array([-0.23207212, -0.12451914, -0.50840576, -0.81985431]), 1: array([ 0.40295376, -0.76601632,  0.0574779 , -0.49753749])}
-        inclination_1, inclination_2, inclination_21, inclination_12 = calculate_inclination(quats[0], quats[1])
-        print("Sensor 1",inclination_1)
-        print("Sensor 2", inclination_2)
-        print("inclination21", inclination_21)
-        print("inclination12", inclination_12)
+        inclination_1, inclination_2, inclination_21= calculate_inclination(quats[0], quats[1])
+        #print("Sensor 1",inclination_1)
+        #print("Sensor 2", inclination_2)
+        #print("inclination21", inclination_21)
         compute_time = time.time() - t
         still_wait = Ts - compute_time
         if still_wait <= 0:
